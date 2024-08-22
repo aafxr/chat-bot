0b0c06e50983a65b7ecb21d4ea02735a93dfea48
aafxr <alexandral.fed23@gmail.com>
2024-08-22 17:58:01 +0700

    backbutton hook

--- src/redux/hooks/useArrowBack.ts
+++ src/redux/hooks/useArrowBack.ts
@@ -1,10 +1,9 @@
 import {useEffect} from "react";
-import {useNavigate} from "react-router";
 
 export function useArrowBack(){
-    const navigate = useNavigate()
     useEffect(() => {
         const tg = Telegram.WebApp
-        tg.BackButton.onClick(() => navigate(-1))
+        tg.BackButton.show()
+        // tg.BackButton.onClick(() => navigate(-1))
     }, []);
 }
